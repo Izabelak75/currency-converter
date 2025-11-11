ea11fcd364c720c8467c6cef9e4c871e3f09960f
Izabela Kuc <isabela.kuc@gmail.com>
2025-11-10 18:20:27 -0800

    modyfied body style

--- JS/script.js
+++ JS/script.js
@@ -1,34 +1,47 @@
 
-console.log("Hello");
+{
+    const calculateResult = (amount, currency) => {
+      const rateEUR = 4.29;
+      const rateCAD = 2.65;
+      const rateUSD = 3.73;
 
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
-
-let rateEUR = 4.29;
-let rateCAD = 2.65;
-let rateUSD = 3.73;
-
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  let amount = +amountElement.value;
-  let currency = currencyElement.value;
-
-  let result;
-  
-  switch (currency) {
-    case "EUR":
-      result = amount / rateEUR;
-      break;
-    case "CAD":
-      result = amount / rateCAD;
-      break;
-    case "USD":
-      result = amount / rateUSD;
-      break;
-    
+      switch (currency) {
+        case "EUR":
+          return amount / rateEUR;
+        case "CAD":
+          return amount / rateCAD;
+        case "USD":
+          return amount / rateUSD;
+     
   }
-  resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
-});
+};
+
+ const updateResult = (result, currency) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerHTML = `Wynik to: <strong>${result.toFixed(2)} ${currency}</strong>`
+    }
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+
+         const result = calculateResult(amount, currency);
+
+        updateResult(result, currency);
+    };
+
+    const init = () => {
+        const formElement = document.querySelector(".js-form");
+
+        formElement.addEventListener("submit", onFormSubmit);
+    };
+
+    init();
+}
+
+
+
+
